1d6e878e8d1ba3e26ce30452b34efaa3743c34e4
petadox <jesus.gimenez@est.fib.upc.edu>
2016-05-24 19:45:37 +0200

    Import no need
    josputa

--- src/Interficie/Grafo8.java
+++ src/Interficie/Grafo8.java
@@ -4,7 +4,6 @@ import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import javax.swing.JFrame;
